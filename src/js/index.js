8e1a84c5d2b4109169dd3ef94e0945963319b207
valiass <mvalias521@gmail.com>
2024-08-08 14:35:20 -0300

    V1.1

--- src/js/index.js
+++ src/js/index.js
@@ -1,76 +1,71 @@
+/*
 
-//  OBJETIVO - quando clicarmos na aba, temos que mostrar o 
-//  conteúdo da aba que foi clicada pelo usuário
-//  e esconder  o conteúdo da aba anterior
+OBJETIVO 1 - QUANDO O USUÁRIO CLICAR NA SETA AVANÇAR, IR PARA A PRÓXIMA CARTA...
 
-//  - passo 1 - dar um jeito de pegar os elementos que
-//  representam as abas no HTML
+    passo 1 - pegar o elemento HTML da seta avançar
 
-//  - passo 2 - dar um jeito de identificar o clique
-//  no elemento da aba
+    passo 2 - dar um jeito de identificar quando o usuário clicar nela
 
-//  - passo 3 - quando o usúario clicar, desmarcar
-//  a aba selecionada
+    passo 3 - mudar para a próxima carta da lista
 
-//  - passo 4 - marcar a aba clicada como selecionada
-
-//  - passo 5 - esconder o conteúdo anterior
-
-//  - passo 6 - mostrar o conteúdo da aba selecionada
+    passo 4 - esconder a carta que estava selecionada
 
 
+OBJETIVO 2 - QUANDO O USUÁRIO CLICAR NA SETA VOLTAR, IR PARA A CARTA ANTERIOR...
 
-//  passo 1 - dar um jeito de pegar os elementos que representam as abas no HTML
-//  console.log(document.querySelectorAll(".aba"));
- const abas = document.querySelectorAll(".aba")
-//  console.log(abas);
+    passo 1 - pegar o elemento HTML da seta voltar
 
-//  - passo 2 - dar um jeito de identificar o clique no elemento da aba
- abas.forEach(aba => { 
+    passo 2 - dar um jeito de identificar quando o usuário clicar nela
 
-    aba.addEventListener("click", function(){
+    passo 3 - mudar para a carta anterior da lista
 
-     if (aba.classList.contains("selecionada")){
-         return
-     }
-        //   console.log("não estava selecionada")
+    passo 4 - esconder a carta que estava selecionada
 
-      //    console.log("clicou na aba", aba);
 
-        selecionarAba(aba)
-        mostrarInformacao(aba)
+*/
 
-      })
- }) 
+// passo 1 - pegar o elemento HTML da seta avançar
+
+const btnAvancar = document.getElementById("seta-avancar");
+const btnVoltar = document.getElementById("seta-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado(){
+  const mostrarCartaoSelecionado = document.querySelector(".selecionado");
+  mostrarCartaoSelecionado.classList.remove("selecionado");
+
+}
+
+function mostrarCartaoSelecionado(){
+  cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+
+btnAvancar.addEventListener("click", function (){
+
+   if(cartaoAtual === cartoes.length - 1) {
+     return
+   }
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual++;
+
+    mostrarCartaoSelecionado();
   
-    function selecionarAba(aba){
-   
-     //  - passo 3 - quando o usúario clicar,desmarcar a aba selecionada
-      //   
-      const abaSelecionada = document.querySelector(".aba.selecionada")
-      // console.log(abaSelecionada);
-   abaSelecionada.classList.remove("selecionada")
+});
 
-      //    - passo 4 - marcar a aba clicada como 
-      // selecionada 
-   aba.classList.add("selecionada");
-}
+btnVoltar.addEventListener("click", function () {
+   if(cartaoAtual <= 0){
+    return
+  }
 
-   function mostrarInformacao(aba){
+  esconderCartaoSelecionado();
 
-    // - passo 5 - esconder o conteúdo anterior
-    const informacaoSelecionada = document.querySelector(".informacao.selecionada");
-    //   console.log(informacaoSelecionada)
-    informacaoSelecionada.classList.remove("selecionada")
+  cartaoAtual--;
 
-    // - passo 6 - mostrar o conteúdo da aba selecionada
-    
-    //   console.log(aba.id)
-      const idDoElementoDaInformacaoDaAba = `informacao-${aba.id}`;
-    //   console.log(idDoElementoDaInformacaoDaAba)
+  mostrarCartaoSelecionado();
 
-     const informacaoASerMostrada = document.getElementById(idDoElementoDaInformacaoDaAba);
-     informacaoASerMostrada.classList.add("selecionada");
-}
-
+});
 
